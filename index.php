434ff83ccccc00c78725313d9c8698564aabca27
app83 <45634002+app83@users.noreply.github.com>
2020-12-15 19:37:35 -0500

    Update index.php

--- index.php
+++ index.php
@@ -103,10 +103,12 @@ switch ($action) {
     }
     case 'display_edit_question': {
         //display the edit question form
-        $userId = filter_input(INPUT_GET, 'userId');
+        $userId = filter_input(INPUT_POST, 'userId');
+        $questionId = filter_input(INPUT_POST, 'questionId');
         if ($userId == NULL || $userId < 0) {
             header('Location: .?action=show_login');
         } else {
+            $question = get_question($questionId);
             include('views/edit_question_form.php');
         }
         break;
@@ -124,8 +126,8 @@ switch ($action) {
             include('errors/error.php');
         } else {
            edit_question($questionId, $title, $body, $skills);
-           header("Location: .?action=display_edit_question&userId=$userId");
-           //header("Location: .?action=display_questions&userId=$userId");
+            //header("Location: .?action=display_edit_question&userId=$userId");
+           header("Location: .?action=display_questions&userId=$userId");
         }
         break;
     }
